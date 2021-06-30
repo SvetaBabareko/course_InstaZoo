c21260729bbc6deffdae7ce7310658e063f18448
Светлана <sveta.romashko@inbox.ru>
2021-06-30 23:59:21 +0300

    modify user model

--- src/main/java/com/example/entity/BaseEntity.java
+++ src/main/java/com/example/entity/BaseEntity.java
@@ -1,10 +1,19 @@
 package com.example.entity;
 
-import javax.persistence.PrePersist;
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import javax.persistence.*;
+
 import java.time.LocalDateTime;
 
+@MappedSuperclass
 public class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     protected Long id;
+
+    @JsonFormat(pattern = "yyyy-mm-dd HH:mm:ss")
+    @Column(updatable = false)
     protected LocalDateTime createdDate;
 
     @PrePersist
--- src/main/java/com/example/entity/User.java
+++ src/main/java/com/example/entity/User.java
@@ -1,24 +1,42 @@
 package com.example.entity;
 
 import com.example.entity.enums.ERole;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
 
-import javax.persistence.PrePersist;
+import javax.persistence.*;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
+@Data
+@Entity
+@NoArgsConstructor
 public abstract class User extends BaseEntity{
-
+    @Column(nullable = false)
     private String name;
+    @Column(unique = true, updatable = false)
     private String username;
+    @Column(nullable = false)
     private String lastname;
+    @Column(unique = true)
     private String email;
+    @Column(columnDefinition = "text")
     private String bio;
+    @Column(length = 3000)
     private String password;
 
+    @ElementCollection(targetClass = ERole.class)
+    @CollectionTable(name = "user_role",
+    joinColumns = @JoinColumn("user_id"))
     private Set<ERole> role = new HashSet<>();
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "user", orphanRemoval = true)
     private List<Post> posts = new ArrayList<>();
+
+    @Transient
+    private Collection<? extends GrantedAuthority> authorities;
+
+
 
 }
