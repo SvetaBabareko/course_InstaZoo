51533f7815ead8fb84064b0517d0c5e8e485302a
Светлана <sveta.romashko@inbox.ru>
2021-07-01 00:05:56 +0300

    add annotations for post

--- src/main/java/com/example/entity/Post.java
+++ src/main/java/com/example/entity/Post.java
@@ -1,21 +1,32 @@
 package com.example.entity;
 
+import lombok.Data;
+
+import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-public class Post {
-    private Long id;
+@Entity
+@Data
+public class Post extends BaseEntity{
+
     private String title;
     private String caption;
     private String location;
     private Integer likes;
 
+    @Column
+    @ElementCollection(targetClass = String.class)
     private Set<String> likedUsers = new HashSet<>();
+
+    @ManyToOne(fetch = FetchType.LAZY)
     private User user;
+
+    @OneToMany(cascade = CascadeType.REFRESH,fetch = FetchType.EAGER, mappedBy = "post", orphanRemoval = true)
     private List<Comment> comments = new ArrayList<>();
-    private LocalDateTime createdDate;
+
 
 }
