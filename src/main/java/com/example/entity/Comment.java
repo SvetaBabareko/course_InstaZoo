532af1db32ce2fa2f5ed7a896ac25a37939ad786
Светлана <sveta.romashko@inbox.ru>
2021-07-01 00:09:18 +0300

    add annotations for comment entity

--- src/main/java/com/example/entity/Comment.java
+++ src/main/java/com/example/entity/Comment.java
@@ -1,8 +1,21 @@
 package com.example.entity;
 
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.ManyToOne;
+
+@Data
+@Entity
 public class Comment extends BaseEntity{
+    @ManyToOne(fetch = FetchType.EAGER)
     private Post post;
+    @Column(nullable = false)
     private String username;
+    @Column(nullable = false)
     private Long userId;
+    @Column(nullable = false, columnDefinition = "test")
     private String message;
 }
